34998175896fe636e39e93043c502fef814ac33a
João Fernando Cosme Bolzani Cheroto <154237716+fjoao9744@users.noreply.github.com>
2024-10-14 12:56:27 -0300

    criando placeholder

---
+++ desktop/prototypes/prototype02.py
@@ -0,0 +1,41 @@
+from tkinter import *
+from supabase import create_client
+
+url = "https://yurkilcutxjmzhbiojkf.supabase.co"
+key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl1cmtpbGN1dHhqbXpoYmlvamtmIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcyNzgwNjM0OSwiZXhwIjoyMDQzMzgyMzQ5fQ.MqPS9cCgjrCYZACY9zQZuQsBPV98ZZCo6488Sh_wZgk"
+
+screen = Tk()
+screen.geometry("800x500")
+
+def name_save(event=None):
+    pass
+
+class Name_PlaceHolder():
+    def view(event=None):
+        if name_entry.get() == "":
+            name_entry.insert(0, "Digite seu nome...")
+
+    def clear(event=None):
+        if name_entry.get() == "Digite seu nome...":
+            name_entry.delete(0, "end")
+
+name_entry = Entry(screen, width=40)
+name_entry.grid(row=0, column=0)
+name_entry.insert(0, "Digite seu nome...")
+name_entry.bind("<FocusOut>", Name_PlaceHolder.view, add="+")
+name_entry.bind("<FocusIn>", Name_PlaceHolder.clear, add="+")
+
+
+msg_entry = Entry(screen, width=40)
+msg_entry.grid(row=1, column=0)
+
+
+name_button = Button(screen, text="salvar nome", bg="green", command=name_save)
+name_button.grid(row=0, column=1)
+
+
+print(name_entry.get())
+
+
+
+mainloop()
