e909fe930b82dd7183f3709a270e460f927d3e9b
João Fernando Cosme Bolzani Cheroto <154237716+fjoao9744@users.noreply.github.com>
2024-11-16 21:05:02 -0300

    função de salvar o nome

--- web/prototypes/prototype03/main.py
+++ web/prototypes/prototype03/main.py
@@ -1,12 +1,24 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request
 from connect import messages
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def homepage():
+    is_name = False
+    name = ""
+    if request.method == "POST":
+        action = request.form.get("action")
 
-    return render_template("main.html", messages=messages.data)
+        if action == "save":
+            name = request.form.get("name")
+            is_name = True
+
+        if action == "edit":
+            pass
+
+    return render_template("main.html", messages = messages.data, NOME = name, IS_NAME=is_name)
+
 
 if __name__ == '__main__':
     app.run(debug = True) 
